0f214f28c26b614e94b3710ffbb649d4131d5202
Carlos Vargas <carlos.vargas.7e8@itb.cat>
2025-11-10 23:21:54 +0100

    ejercicio acabado

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
+end = False
+botonA = False
+botonB = False
+tem = 0
+x = 2
+y = 2
 
+
+def on_button_pressed_a():
+    global botonA, botonB
+    botonB = False
+    botonA = True
+   
+    
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global  botonA, botonB
+    botonA = False
+    botonB = True
+   
+   
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    global tem, botonA, botonB, x, y
+    tem = input.temperature()
+    if botonA: 
+        led.plot_bar_graph(tem, 50, True)
+    elif botonB:
+        
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+       
+        # Movimiento horizontal (X)
+        if accX <= 150 and x > 0:
+            x = x - 1
+        elif accX > 150 and x < 4:
+            x = x + 1
+
+        # Movimiento vertical (Y)
+        if accY <= 150 and y > 0:
+            y = y - 1
+        elif accY > 150 and y < 4:
+            y = y + 1
+        basic.clear_screen()
+        led.plot(x, y)
+        basic.pause(100)
+
+        
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,46 @@
-
+let end = false
+let botonA = false
+let botonB = false
+let tem = 0
+let x = 2
+let y = 2
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    botonB = false
+    botonA = true
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    botonA = false
+    botonB = true
+})
+basic.forever(function on_forever() {
+    let accX: number;
+    let accY: number;
+    
+    tem = input.temperature()
+    if (botonA) {
+        led.plotBarGraph(tem, 50, true)
+    } else if (botonB) {
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        //  Movimiento horizontal (X)
+        if (accX <= 150 && x > 0) {
+            x = x - 1
+        } else if (accX > 150 && x < 4) {
+            x = x + 1
+        }
+        
+        //  Movimiento vertical (Y)
+        if (accY <= 150 && y > 0) {
+            y = y - 1
+        } else if (accY > 150 && y < 4) {
+            y = y + 1
+        }
+        
+        basic.clearScreen()
+        led.plot(x, y)
+        basic.pause(100)
+    }
+    
+})
